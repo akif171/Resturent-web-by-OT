9c5a34c0ea4df0a1749f64b6e13018704ab89e7c
Muhammad Akif <57750292+akif171@users.noreply.github.com>
2021-06-25 16:44:02 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+
+window.addEventListener("scroll",()=>{
+    
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky",window.scrollY > 0)
+    
+    
+}
+)
+const menuToggle = document.querySelector(".menuToggle");
+const navigation = document.querySelector(".navigation")
+
+menuToggle.addEventListener("click",()=>{
+
+    menuToggle.classList.toggle("active")
+    navigation.classList.toggle("active")
+})
+
+
